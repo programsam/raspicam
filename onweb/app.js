15ca8d1c852369849338e2dc583b5000f9f6a6d7
programsam <programsam@gmail.com>
2015-10-15 02:59:16 -0400

    Serve up the images publically

--- onweb/app.js
+++ onweb/app.js
@@ -4,13 +4,13 @@ var multer = require('multer')
 
 var upload = multer({dest: 'files/'})
 
+express.static(__dirname + "/files"
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.post('/upload', upload.single('file'), function (req, res) {
 	res.json({"message":"OK"})
-	console.log(JSON.stringify(req.files))
 })
 
 var server = app.listen(3090, function () {

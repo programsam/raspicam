af82c09ed0a8809b52e88379978a83eeb890abdd
programsam <programsam@gmail.com>
2015-10-15 02:47:15 -0400

    Receiving end should look better.

--- onweb/app.js
+++ onweb/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var multer = require('multer')
+
+var upload = multer({dest: 'files/'})
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/upload', function (req, res) {
+app.post('/upload', upload.single('file'), function (req, res) {
 	res.json({"message":"OK"})
-	console.log(req.files)
+	console.log(JSON.stringify(req.files))
 })
 
 var server = app.listen(3090, function () {

807bfda761bb6ff5152ae6f0d3729b0501659b97
programsam <programsam@gmail.com>
2015-10-15 02:22:21 -0400

    Get verbose with events.

--- onpi/app.js
+++ onpi/app.js
@@ -7,28 +7,20 @@ options = {
 
 var camera = new RaspiCam(options);
 
-//to take a snapshot, start a timelapse or video recording
 camera.start();
 
-//to stop a timelapse or video recording
-camera.stop();
-
-//listen for the "start" event triggered when the start method has been successfully initiated
 camera.on("start", function(){
-    //do stuff
+    console.log("Started taking picture...")
 });
 
-//listen for the "read" event triggered when each new photo/video is saved
 camera.on("read", function(err, timestamp, filename){ 
-    //do stuff
+    console.log("Reading picture...")
 });
 
-//listen for the "stop" event triggered when the stop method was called
 camera.on("stop", function(){
-    //do stuff
+    console.log("Stopping...")
 });
 
-//listen for the process to exit when the timeout has been reached
 camera.on("exit", function(){
-    //do stuff
+    console.log("Timeout has beenr eached")
 });

319ff58fd97dc77c81ff95b78d9a690fbb484f9c
programsam <programsam@gmail.com>
2015-10-15 07:57:36 -0400

    List the things in the folder we want.

--- onpi/capture.js
+++ onpi/capture.js
@@ -10,6 +10,16 @@ var s3 = new AWS.S3({
 	secretAccessKey: settings.s3.secret_key
 })
 
+var params = {
+  Bucket: 'bensmith',
+  Prefix: '/dropcam'
+};
+s3.listObjects(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
+
+
 options = {
 	mode: "photo",
 	output: "./pics/cam.jpg",

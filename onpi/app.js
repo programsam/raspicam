e8b247470a7b38b1401c29acdc29a06f075a8188
programsam <programsam@gmail.com>
2015-10-15 02:27:07 -0400

    Get a little better maybe.

--- onpi/app.js
+++ onpi/app.js
@@ -2,19 +2,19 @@ var RaspiCam = require("raspicam");
 
 options = {
 	mode: "photo",
-	output: "./pics/cam.jpg"
+	output: "./pics/cam.jpg",
+	rot: 180
 }
 
 var camera = new RaspiCam(options);
 
-camera.start();
 
 camera.on("start", function(){
     console.log("Started taking picture...")
 });
 
 camera.on("read", function(err, timestamp, filename){ 
-    console.log("Reading picture...")
+    console.log("Reading picture with timestamp " + timestamp + " and filename " + filename)
 });
 
 camera.on("stop", function(){
@@ -22,5 +22,7 @@ camera.on("stop", function(){
 });
 
 camera.on("exit", function(){
-    console.log("Timeout has beenr eached")
+    console.log("Timeout has been reached")
 });
+
+camera.start();

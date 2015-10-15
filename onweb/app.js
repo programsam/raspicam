0a3a29f653adf6225607b53f325191e5e04b58d9
programsam <programsam@gmail.com>
2015-10-15 03:00:09 -0400

    Dank as hell.

--- onweb/app.js
+++ onweb/app.js
@@ -4,13 +4,14 @@ var multer = require('multer')
 
 var upload = multer({dest: 'files/'})
 
-express.static(__dirname + "/files"
+express.static(__dirname + "/files")
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.post('/upload', upload.single('file'), function (req, res) {
 	res.json({"message":"OK"})
+	console.log(JSON.stringify(req.file))
 })
 
 var server = app.listen(3090, function () {

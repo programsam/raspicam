8f75f0218574fc87d4ccb6a36bcf1bc76472b36e
programsam <programsam@gmail.com>
2015-10-15 08:27:21 -0400

    Forgot some preliminaries.

--- onweb/app.js
+++ onweb/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var AWS = require('aws-sdk')
 var app = express();
 
+var settings = require('./settings')
 var s3 = new AWS.S3({
 	accessKeyId: settings.s3.access_key,
 	secretAccessKey: settings.s3.secret_key
